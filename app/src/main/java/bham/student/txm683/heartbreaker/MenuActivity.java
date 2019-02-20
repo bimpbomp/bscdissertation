f598129257457a953f76db4819a9736a5a81a9d7
tobymorgan <tobymorgan97@gmail.com>
2019-02-20 11:26:40 +0000

    started implementing menuActivity

---
+++ app/src/main/java/bham/student/txm683/heartbreaker/MenuActivity.java
@@ -0,0 +1,14 @@
+package bham.student.txm683.heartbreaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
